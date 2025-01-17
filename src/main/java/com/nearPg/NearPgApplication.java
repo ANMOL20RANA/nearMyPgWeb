6948e01756eb606f350f70bde4504eb50ba5d928
Anmol Rana <anmolrana106@gmail.com>
2025-01-17 22:05:07 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/nearPg/NearPgApplication.java
@@ -0,0 +1,13 @@
+package com.nearPg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nearPg/NearPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nearPg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
